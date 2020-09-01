6046502def379fa05e7fa30762c1e44582d6e73b
codetalkertive <codetalkertive@gmail.com>
2020-09-02 00:00:20 +0100

    Sign Up Page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,24 @@
 class StudentsController < ApplicationController
+
+	def new
+		@student = Student.new
+	end
+
+	def create
+		@student = Student.create(student_params)
+
+		@student.student_id.downcase
+
+		if @student.save
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def student_params
+		params.require(:student).permit(:name, :student_id, :password, :password_confirmation)
+	end
 end
